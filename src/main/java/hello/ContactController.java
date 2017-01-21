d0e0b1ade05b2bb09aa04bc02c646cc152a818c1
Leanne <leanne@Leannes-MacBook-Air.local>
2017-01-21 21:15:56 +0000

    contact us form changes

--- src/main/java/hello/ContactController.java
+++ src/main/java/hello/ContactController.java
@@ -4,17 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ContactController {
 
     @RequestMapping(value = "/contact", method = RequestMethod.POST)
     public String submitMessage(@ModelAttribute("SpringWeb")Message message, ModelMap model) {
-        model.addAttribute("phone-number", message.getPhoneNumber());
+        model.addAttribute("phone", message.getPhoneNumber());
         model.addAttribute("email", message.getEmailAddress());
         model.addAttribute("message", message.getMessage());
 
--- src/main/resources/static/js/contact-us.js
+++ src/main/resources/static/js/contact-us.js
@@ -1,22 +1,49 @@
-$( "#contact-us-form" ).submit(function( event ) {
-
-    // Stop form from submitting normally
-    event.preventDefault();
-
-    // Get some values from elements on the page:
-    var $form = $( this ),
-      url = $form.attr( "action" );
-
-    // Send the data using post
-    var posting = $.post(
-            url,
-            $( "#contact-us-form" ).serialize()
-     );
-
-    // Put the results in a div
-    posting.done(function( data )
+$("#contact-us-form").validate({
+    rules: {
+        email: {
+            required: {
+                depends: function(element) {
+                    return !$("#phone").val();
+                }
+            },
+            email: true
+        },
+        message: "required"
+    },
+    messages: {
+        email: {
+          required: "Please provide either a phone number or email address to be contacted on.",
+        }
+    },
+    submitHandler: function(form)
     {
-      var content = $( data ).find( "#content" );
-      $( "#result" ).empty().append( content );
-    });
+        // Stop form from submitting normally
+        event.preventDefault();
+
+        // Get some values from elements on the page:
+        var form = $( "#contact-us-form" );
+        var url = form.attr( "action" );
+        var result = $("#result");
+
+        // Send the data using post
+        var posting = $.post
+        (
+            url,
+            form.serialize()
+        );
+
+        // Put the results in a div
+        posting.done(function( data )
+        {
+            form.slideToggle(1000);
+            result.append( $(data) ).slideToggle(1000);
+        });
+
+        posting.fail(function( data )
+        {
+            errorContent = "<div class=\"error\" >Currently unable to send query. Please call 07860681642</div>";
+            form.slideToggle(1000);
+            result.append( errorContent ).slideToggle(1000);
+        });
+     }
 });
